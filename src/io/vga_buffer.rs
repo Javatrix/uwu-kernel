85d447593453c72994b64b6b308869ce435c4286
Javatrix <javatrix912@gmail.com>
2024-07-19 21:17:02 +0200

    Redo stuff like the book says.

--- src/io/vga_buffer.rs
+++ src/io/vga_buffer.rs
@@ -1,4 +1,30 @@
-use core::isize;
+use lazy_static::lazy_static;
+use spin::Mutex;
+
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -24,10 +50,10 @@ pub enum Color {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(transparent)]
-struct ColorCode(u8);
+pub struct ColorCode(u8);
 
 impl ColorCode {
-    fn new(foreground: Color, background: Color) -> Self {
+    pub fn new(foreground: Color, background: Color) -> ColorCode {
         ColorCode((background as u8) << 4 | (foreground as u8))
     }
 }
@@ -35,35 +61,85 @@ impl ColorCode {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
 struct ScreenChar {
-    ascii_char: u8,
+    ascii_character: u8,
     color_code: ColorCode,
 }
 
-fn vga_buffer() -> *mut u8 {
-    0xb8000 as *mut u8
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+use volatile::Volatile;
+
+pub struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
-pub fn num_to_ascii(number: u8) -> u8 {
-    return (48 + number) as u8;
+pub struct Writer {
+    pub column_position: usize,
+    pub color_code: ColorCode,
+    pub buffer: &'static mut Buffer,
 }
 
-const VGA_WIDTH: u8 = 80;
-const VGA_HEIGHT: u8 = 25;
-static mut CURSOR_POS: u32 = 0;
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
 
-pub fn print(chars: &[u8]) {
-    for &byte in chars {
-        print_screen_char(ScreenChar {
-            ascii_char: byte,
-            color_code: ColorCode::new(Color::White, Color::Cyan),
-        });
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe),
+            }
+        }
     }
 }
 
-fn print_screen_char(char: ScreenChar) {
-    unsafe {
-        *vga_buffer().offset(CURSOR_POS as isize * 2) = char.ascii_char;
-        *vga_buffer().offset(CURSOR_POS as isize * 2 + 1) = char.color_code.0;
-        CURSOR_POS += 1;
+use core::fmt;
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,19 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-
-use io::vga_buffer::{self, num_to_ascii, print};
+use io::vga_buffer;
 
 pub mod io;
-pub mod utils;
-
-static HELLO_WORLD: &[u8] = b"Hello from my custom kernel!";
 
 #[no_mangle]
-pub extern "C" fn _start() {
-    print_boot_message();
-
-    let message: &mut [u8; 10] = &mut [0; 10];
-    for i in 0..10 {
-        message[i] = num_to_ascii(i as u8);
-    }
-    print(message);
+pub extern "C" fn _start() -> ! {
+    println!("YAY, custom {} of values: {}!", "printing", 43.0 / 24.0);
 
     loop {}
 }
 
-fn print_boot_message() {
-    vga_buffer::print(HELLO_WORLD);
-}
-
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
